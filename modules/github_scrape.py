f1bd0fb253a1a4c8c104a683205b4d9f21276f53
Odyhibit <josh.2990@gmail.com>
2022-10-03 09:04:57 -0500

    output directory variable

--- modules/github_scrape.py
+++ modules/github_scrape.py
@@ -3,14 +3,14 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def process_folders_files(page_soup: BeautifulSoup, github: str, last_update: str):
+def process_folders_files(page_soup: BeautifulSoup, github: str, last_update: str, output_dir: str):
     raw_button = page_soup.find("a", {"id": "raw-url"})
     row_list = page_soup.find_all('div', {"role": "row"})
 
     if raw_button:
         filename = page_soup.find("strong", "final-path").text
         raw_content_url = github + page_soup.find('a', id="raw-url")['href']
-        write_file(filename, raw_content_url)
+        write_file(output_dir, filename, raw_content_url)
         print("writing", filename, raw_content_url)
         return
     elif len(row_list) > 0:
@@ -30,14 +30,14 @@ def get_new_soup(url: str) -> BeautifulSoup:
     return BeautifulSoup(r.text, 'html.parser')
 
 
-def write_file(filename: str, content_url: str):
-    with open("github_downloads/" + filename, "wb") as file_out:
+def write_file(output_dir: str, filename: str, content_url: str):
+    with open(output_dir + filename, "wb") as file_out:
         content = requests.get(content_url)
         file_out.write(content.content)
 
 
-
 if __name__ == "__main__":
     github_url = "https://github.com/vxunderground/MalwareSourceCode"
+    output_dir = "github_downloads/"
     last_update = "2020-08-20"
-    process_folders_files(get_new_soup(github_url), "https://github.com", last_update)
+    process_folders_files(get_new_soup(github_url), "https://github.com", last_update, output_dir)
