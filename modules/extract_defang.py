d196f69f6a8893f8f231a29dabbc1e7bdf854ac0
Odyhibit <josh.2990@gmail.com>
2022-10-09 22:38:43 -0500

    removed beautiful soup, and the datetime functionality. GitHub had intermittent errors. Saving downloaded files in a sqlite db, if the file is in the db it will not be downloaded.

--- modules/extract_defang.py
+++ modules/extract_defang.py
@@ -3,39 +3,52 @@ import pathlib
 import subprocess
 
 
-def process_archive(this_archive: pathlib.Path, this_directory: pathlib.Path):
+def process_archive(this_archive: pathlib.Path, this_directory: pathlib.Path, archive_extensions, dangerous_extensions):
     new_dir = str(this_directory) + "/" + pathlib.Path(this_archive).stem.replace(".", "_")
     os.makedirs(new_dir, exist_ok=True)
-    destination = pathlib.Path(new_dir)
-    print(this_archive, "->", pathlib.Path(this_archive.parent, destination))
-    extract = subprocess.run(["7z", "x", "-pinfected", "-o" + new_dir, this_archive])
-    print("7z", "x", "-o" + new_dir, this_archive)
-    if extract.check_returncode() == 0:
+    extract = subprocess.run(["7z", "x", "-y", "-pinfected", "-o" + new_dir, this_archive], capture_output=True)
+    # print("7z", "  x", " -y", " -o" + new_dir, this_archive)
+    print("Extracting", this_archive)
+    if extract.returncode == 0:
+        print(in_green("Success"))
+        if pathlib.Path(this_archive).is_file():
+            extract_defang(pathlib.Path(new_dir), archive_extensions, dangerous_extensions)
         os.remove(this_archive)
-        extract_defang(pathlib.Path(new_dir))
     else:
-        print("ERROR extracting", this_archive)
+        print(in_red("ERROR"), "extracting", this_archive, "return code", extract.returncode)
 
 
 def make_benign(this_file: pathlib.Path):
     os.rename(this_file, str(this_file) + "_")
+    if pathlib.Path(this_file).is_file():
+        cwd = os.getcwd()
+        os.remove(cwd + "/" + str(this_file))
 
 
-def extract_defang(this_directory: pathlib.Path):
+def extract_defang(this_directory: pathlib.Path, archive_extensions:[str], dangerous_extensions:[str]):
     dir_list = pathlib.Path(this_directory)
     for item in dir_list.iterdir():
         if item.is_dir():
-            extract_defang(item)
+            extract_defang(item, archive_extensions, dangerous_extensions)
         if item.is_file():
             if pathlib.Path(item).suffix in archive_extensions:
-                process_archive(item, this_directory)
+                process_archive(item, this_directory, archive_extensions, dangerous_extensions)
             if pathlib.Path(item).suffix in dangerous_extensions:
                 make_benign(item)
 
 
+def in_green(text: str) -> str:
+    return "\033[32;1m" + str(text) + "\033[0m"
+
+
+def in_red(text: str) -> str:
+    return "\033[31m" + str(text) + "\033[0m"
+
+
+
 if __name__ == "__main__":
-    archive_extensions = [".gz", ".zip", ".rar", ".7z"]
-    dangerous_extensions = [".exe", ".bat", ".ps1", ".css", ".html", ".htm", ".LNK", ".VBE", ".cmd", ".sh",
+    archive_ext = [".gz", ".zip", ".rar", ".7z"]
+    dangerous_ext = [".exe", ".bat", ".ps1", ".css", ".html", ".htm", ".LNK", ".VBE", ".cmd", ".sh",
                             ".js", ".vb", ".vbs", ".jar", ".doc", ".docm", ".docx", ".eml", ".ini", ".msi",
                             ".ppt", ".xls", ".xlsx", ".xlsm"]
-    extract_defang(pathlib.Path("../github_downloads"))
+    extract_defang(pathlib.Path("../test/test_downloads"), archive_ext, dangerous_ext)
--- modules/scrape_github.py
+++ modules/scrape_github.py
@@ -1,33 +1,62 @@
 import os
-from datetime import datetime, timezone
+import sqlite3
+import urllib.parse
 import requests
-from bs4 import BeautifulSoup
 
 
-def scrape_github(page_soup: BeautifulSoup, last_update: str, output_dir: str):
-    raw_button = page_soup.find("a", {"id": "raw-url"})
-    row_list = page_soup.find_all('div', {"role": "row"})
+def scrape_github(url: str, connection: sqlite3.Connection, db_cur: sqlite3.Cursor, output_dir: str):
     github = "https://github.com/"
-    if raw_button:
-        filename = raw_button['href']
-        raw_content_url = github + page_soup.find('a', id="raw-url")['href']
-        write_file(output_dir, filename, raw_content_url)
-        print(filename, raw_content_url)
+    page_source = requests.get(url).text
+
+    raw_button = get_raw_button(page_source)
+    file_list = get_file_list(page_source)
+
+    if raw_button != "":
+        filename = raw_button
+        if not in_db(filename, db_cur):
+            insert_new(filename, db_cur)
+            write_file(output_dir, filename, github + filename)
+            connection.commit()
+            print(filename)
         return
-    elif len(row_list) > 0:
-        for row in row_list:
-            url_link = row.find("a", "js-navigation-open Link--primary")
-            update_timestamp = row.find("time-ago", "no-wrap")
-            if url_link and update_timestamp and update_timestamp['datetime'] > last_update:
-                scrape_github(get_new_soup(github + url_link['href']), last_update, output_dir)
-                # print(url_link['href'], update_timestamp['datetime'],last_update,(update_timestamp['datetime'] > last_update))
+    elif len(file_list) > 0:
+        for file in file_list:
+            scrape_github("https://github.com" + file, connection, db_cur, output_dir)
+
     else:
         print("no links found")
 
 
-def get_new_soup(url: str) -> BeautifulSoup:
-    r = requests.get(url)
-    return BeautifulSoup(r.text, 'html.parser')
+def insert_new(filename: str, cursor: sqlite3.Cursor):
+    cursor.execute('INSERT INTO filenames(name) VALUES(?)', (filename,))
+    return
+
+
+def in_db(filename: str, cursor: sqlite3.Cursor) -> bool:
+    cursor.execute('SELECT count(name) FROM filenames WHERE  name = ? ', (filename,))
+    found = cursor.fetchone()
+    if found[0] > 0:
+        return True
+    return False
+
+
+def get_raw_button(page_source: str) -> str:
+    end = page_source.find('" id="raw-url"')
+    if not end > 0:
+        return ""
+    start = page_source.rfind('href="', 0, end) + 6
+    return urllib.parse.unquote(page_source[start:end])
+
+
+def get_file_list(page_source: str) -> [str]:
+    file_list = []
+    while "js-navigation-open Link--primary" in page_source:
+        link_class = page_source.find("js-navigation-open Link--primary")
+        start = page_source.find('href="', link_class) + 6
+        end = page_source.find('"', start)
+        file_list.append(urllib.parse.unquote(page_source[start:end]))
+        page_source = page_source[end:]
+    return file_list
 
 
 def get_folder(filename: str) -> str:
@@ -35,25 +64,26 @@ def get_folder(filename: str) -> str:
 
 
 def write_file(output_dir: str, filename: str, content_url: str):
+    filename = urllib.parse.unquote(filename)
     os.makedirs(output_dir + get_folder(filename), exist_ok=True)
     with open(output_dir + filename, "wb") as file_out:
         content = requests.get(content_url)
         file_out.write(content.content)
 
 
-def get_last_update(filename: str) -> str:
-    with open(filename, "r") as update:
-        return update.readline().strip()
-
-
-def set_last_update(filename: str):
-    with open(filename, "w") as output:
-        github_dt = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
-        output.write(github_dt)
-
-
 if __name__ == "__main__":
+    con = sqlite3.connect('../github_files.db')
+    cur = con.cursor()
+    print(in_db("test", cur))
+    github_url = "https://github.com/Odyhibit/nested_compressed_folders"
+    # test = requests.get(github_url)
+    files = get_file_list(github_url)
+    for item in files:
+        print(item)
+    '''
+    sqlite_connection = get_connection("../github_files.db")
     github_url = "https://github.com/vxunderground/MalwareSourceCode"
     output_dir = "../github_downloads/"
     last_update = "2020-08-20"
-    scrape_github(get_new_soup(github_url), last_update, output_dir)
+    scrape_github(github_url, sqlite_connection, output_dir)
+    '''
